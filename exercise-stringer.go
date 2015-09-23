31a0f2a37b722613795c1f3b29c65d25183cc598
Yoshiyuki Kondo <cond@st.rim.or.jp>
2015-09-23 11:52:03 +0900

    Exercise: Maps
    https://tour.golang.org/moretypes/19

---
+++ exercise-stringer.go
@@ -0,0 +1,24 @@
+// -*- tab-width:4 -*-
+// Exercise: Stringers
+//
+//    https://tour.golang.org/methods/7
+
+package main
+
+import "fmt"
+
+type IPAddr [4]byte
+
+func (p IPAddr) String() string {
+	return fmt.Sprintf("%v.%v.%v.%v", p[0], p[1], p[2], p[3]);
+}
+
+func main() {
+	addrs := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+	for n, a := range addrs {
+		fmt.Printf("%v: %v\n", n, a)
+	}
+}
